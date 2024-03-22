8fb8459b5de70fc09ce45eaa7306937bfbc88b75
Akash <akkyjat601@gmail.com>
2024-03-22 11:50:25 +0530

    Setup for job recruitment website

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
+import ejs from "ejs";
+import path from "path";
 
 const app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(path.resolve(), "src", "view"));
+
 app.get("/", (req, res) => {
-    res.send("Hello this is home page");
+    res.render("userLayout.ejs");
+});
+app.get("/recruit", (req, res) => {
+    res.render("recruiterLayout.ejs");
 });
 
 export { app };
