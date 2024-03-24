904a41634d73164e7fe5ab14446ae2d8499ef5f4
Akash <akkyjat601@gmail.com>
2024-03-24 15:27:05 +0530

    layout setup

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import ejs from "ejs";
 import path from "path";
+import ejsLayouts from "express-ejs-layouts";
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(ejsLayouts);
 app.use("/node_modules", express.static("node_modules"));
 app.set("view engine", "ejs");
 app.set("views", path.join(path.resolve(), "src", "view"));
 
 app.get("/", (req, res) => {
-    res.render("userLayout.ejs");
+    res.render("userContent", { layout: "userLayout" });
 });
 app.get("/recruit", (req, res) => {
     res.render("recruiterLayout.ejs");
